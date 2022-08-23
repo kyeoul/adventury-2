f7a0e0cadcd45ed6824090857cd26f0fe63f3365
kyeoul <54390304+kyeoul@users.noreply.github.com>
2022-08-23 16:34:12 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import Landing from './Components/Landing';
 import Slides from './Components/Slides';
 import Activities from './Components/Activities';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App font-font_main">
       <Navbar />
-      <Landing />
-      <Activities />
-      <Slides />
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Landing />
+              <Activities />
+              <Slides />
+            </div>
+          } />
+          <Route path="/signup" element={<></>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="w-full p-6 flex flex-col md:flex-row md:justify-between items-center text-xl border-b-2 border-blue-200">
         navbar
 
-        <div className="w-full md:w-auto flex flex-row mt-2 md:mt-0 justify-evenly md:justify-start">
-            <button className="sub-button">Account</button>
-            <button className="sub-button">Payment</button>
-            <button className="sub-button md:signup-button">Sign Up</button>
+        <div className="w-full md:w-auto flex flex-row mt-2 md:mt-0 justify-evenly md:justify-start items-center">
+            <Router>
+              <Link to="/account" className="h-fit"><button className="sub-button">Account</button></Link>
+              <Link to="/payment" className="h-fit"><button className="sub-button">Payment</button></Link>
+              <Link to="/signup"><button className="sub-button md:signup-button">Sign Up</button></Link>
+            </Router>
         </div>
     </div>
   )
