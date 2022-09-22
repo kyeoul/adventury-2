a2e31f9071c3fe0b6014911a44fa8c875aaf3755
ryanz2021 <92238444+ryanz2021@users.noreply.github.com>
2022-09-22 15:58:38 -0500

    update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    phoneNumber: {type: String, required: true}
 }, {
     timestamps: true,
 });
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY);
+}
 
 const User = mongoose.model('User', userSchema);
 
