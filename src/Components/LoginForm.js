4b31a54b9ed334991f99c2d188fa8b129f921a10
kyeoul <54390304+kyeoul@users.noreply.github.com>
2022-09-24 15:33:19 -0500

    Updates to login

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -16,9 +16,9 @@ function LoginForm() {
     return (
         <div className="w-full box-border">
             <form>
-                <input type="text" value={username} onChange={changeUsernameHandler} className="h-12 w-full p-2" placeholder="Username"></input>
+                <input type="text" value={username} onChange={changeUsernameHandler} className="login-textbox" placeholder="Username"></input>
 
-                <input type="text" value={password} onChange={changePasswordHandler} className="h-12 w-full p-2 mt-2" placeholder="Password"></input>
+                <input type="password" value={password} onChange={changePasswordHandler} className="login-textbox mt-2" placeholder="Password"></input>
 
                 <div className="flex justify-end">
                     <button type="submit" className="mt-4 px-4 py-2 bg-white hover:bg-cyan-100 border-2 border-cyan-500 rounded-lg duration-200">Login</button>
