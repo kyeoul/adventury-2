39706d0510cada8bc3badc4abbbba065b2c46dc8
ryanz2021 <92238444+ryanz2021@users.noreply.github.com>
2022-10-01 21:15:46 -0500

    Update LoginForm.js

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -13,10 +13,35 @@ function LoginForm() {
         setPassword(event.target.value)
     }
 
+    async function loginUser(event) {
+        event.preventDefault();
+
+        const response = await fetch('http://localhost:5000/users/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        })
+        
+        const data = await response.json()
+
+        console.log(data)
+    }
+
     return (
         <div className="w-full box-border">
+<<<<<<< Updated upstream
             <form>
                 <input type="text" value={username} onChange={changeUsernameHandler} className="h-12 w-full p-2" placeholder="Username"></input>
+=======
+            <form onSubmit = {loginUser}>
+                <input type="text" value={username} onChange={changeUsernameHandler} className="login-textbox" placeholder="Username"></input>
+>>>>>>> Stashed changes
 
                 <input type="text" value={password} onChange={changePasswordHandler} className="h-12 w-full p-2 mt-2" placeholder="Password"></input>
 
