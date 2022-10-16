e1fa8a9f1d94bb4b72d0f4c8782dae519aecf5ee
kyeoul <54390304+kyeoul@users.noreply.github.com>
2022-10-15 22:53:17 -0500

    Signup page completed

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -21,7 +21,7 @@ function LoginForm() {
                 <input type="password" value={password} onChange={changePasswordHandler} className="login-textbox mt-2" placeholder="Password"></input>
 
                 <div className="flex justify-end">
-                    <button type="submit" className="mt-4 px-4 py-2 bg-white hover:bg-cyan-100 border-2 border-cyan-500 rounded-lg duration-200">Login</button>
+                    <button type="submit" className="mt-4 px-4 py-2 bg-white hover:bg-cyan-100 border-2 border-cyan-500 rounded-lg duration-200">Sign up</button>
                 </div>
             </form>
         </div>
--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -5,25 +5,30 @@ import {useState} from "react"
 function SignupForm() {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    // The input form responses are stored in the data variable as an object
     const {data, setData} = useState({});
     const onSubmit = (formData) => {
         console.log(formData); // an object of form Data
         setData(formData);
     }
 
-    let input_names = [];
+    let input_names = ["Name", "Email", "Phone Number", "Size", "City, State"];
 
     return (
         <div>
             <div className="w-full box-border">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                name="a"
-                placeholder="a..."
-                type="text"
-                className="login-textbox"
-                {...register('message',{required:true})}
-        />
+
+                    {input_names.map((value, index) => (
+                        <input
+                        name={value}
+                        placeholder={value + "..."}
+                        type="text"
+                        className="login-textbox"
+                        {...register('message',{required:true})}
+                        />
+                    ))}
+
                     <div className="flex justify-end">
                         <button type="submit" className="mt-4 px-4 py-2 bg-white hover:bg-cyan-100 border-2 border-cyan-500 rounded-lg duration-200">Login</button>
                     </div>
